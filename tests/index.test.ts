dacd506cf2942a6d4c93118d2be3d146671b98de
Mohamed Ahmed <mohamed.ahmed@ns.nl>
2025-01-22 07:33:52 +0100

    Implement test of hasFreeCell given full placed board

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -18,36 +18,43 @@ describe('tic tac toe game', () => {
     });
   });
   describe('start the game', () => {
-    it('given vertical line of X then return PLAYER X WON!', () => {
-      const game = new TicTacToeGame(verticalLineOfXBoard);
-      expect(game.play()).toStrictEqual('PLAYER X WON!');
+    describe('make a random move for every player', () => {
+      it('given full placed board then the move is impossible ', () => {
+        const game = new TicTacToeGame(drawStateBoard);
+        expect(game.hasFreeCell('O')).toBeFalsy();
+      });
     });
-    it('given vertical line of O then return PLAYER O WON!', async () => {
-      const game = new TicTacToeGame(verticalLineOfOBoard);
-      expect(game.play()).toStrictEqual('PLAYER O WON!');
-    });
+    describe('check the game states', () => {
+      it('given vertical line of X then return PLAYER X WON!', () => {
+        const game = new TicTacToeGame(verticalLineOfXBoard);
+        expect(game.play()).toStrictEqual('PLAYER X WON!');
+      });
+      it('given vertical line of O then return PLAYER O WON!', async () => {
+        const game = new TicTacToeGame(verticalLineOfOBoard);
+        expect(game.play()).toStrictEqual('PLAYER O WON!');
+      });
 
-    it('given horizontal line of X then return PLAYER X WON!', () => {
-      const game = new TicTacToeGame(horizontalLineOfXBoard);
-      expect(game.play()).toStrictEqual('PLAYER X WON!');
-    });
-    it('given horizontal line of O then return PLAYER O WON!', () => {
-      const game = new TicTacToeGame(horizontalLineOfOBoard);
-      expect(game.play()).toStrictEqual('PLAYER O WON!');
-    });
+      it('given horizontal line of X then return PLAYER X WON!', () => {
+        const game = new TicTacToeGame(horizontalLineOfXBoard);
+        expect(game.play()).toStrictEqual('PLAYER X WON!');
+      });
+      it('given horizontal line of O then return PLAYER O WON!', () => {
+        const game = new TicTacToeGame(horizontalLineOfOBoard);
+        expect(game.play()).toStrictEqual('PLAYER O WON!');
+      });
+      it('given diagonal line of X then return PLAYER X WON!', () => {
+        const game = new TicTacToeGame(diagonalLineOfXBoard);
+        expect(game.play()).toStrictEqual('PLAYER X WON!');
+      });
+      it('given diagonal line of O then return PLAYER O WON!', () => {
+        const game = new TicTacToeGame(diagonalLineOfOBoard);
+        expect(game.play()).toStrictEqual('PLAYER O WON!');
+      });
 
-    it('given diagonal line of X then return PLAYER X WON!', () => {
-      const game = new TicTacToeGame(diagonalLineOfXBoard);
-      expect(game.play()).toStrictEqual('PLAYER X WON!');
-    });
-    it('given diagonal line of O then return PLAYER O WON!', () => {
-      const game = new TicTacToeGame(diagonalLineOfOBoard);
-      expect(game.play()).toStrictEqual('PLAYER O WON!');
-    });
-
-    it('given draw state board then return THE GAME ENDS WITH A DRAW! ', () => {
-      const game = new TicTacToeGame(drawStateBoard);
-      expect(game.play()).toStrictEqual('THE GAME ENDS WITH A DRAW!');
+      it('given draw state board then return THE GAME ENDS WITH A DRAW! ', () => {
+        const game = new TicTacToeGame(drawStateBoard);
+        expect(game.play()).toStrictEqual('THE GAME ENDS WITH A DRAW!');
+      });
     });
   });
 });
